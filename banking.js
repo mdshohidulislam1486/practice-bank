9cf81e7ecaec914cfdfad063379d5046fadee91b
mdshohidulislam1486 <Shohidul Islam>
2021-08-15 16:48:45 +0600

    bak work done

--- banking.js
+++ banking.js
@@ -18,17 +18,37 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     
 })
 
-const depositInput = document.getElementById('deposit-input');
-const depositValue = depositInput.value;
-if(depositInput == ' '){
-    const depositBtn = document.getElementById('deposit-btn');
-    depositBtn.setAttribute('disabled', faltruese)
-}else if(depositInput >= 0){
-    depositBtn.setAttribute('disabled', false)
-}
- else {
-    depositBtn.setAttribute('disabled', true)
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+    const withDrawBanalceText = document.getElementById('withdraw-box');
+    const currentWithdraw = withDrawBanalceText.innerText;
+    const withdraConvetoNum =parseFloat(currentWithdraw);
+    const withdrayAmoutotNum= parseFloat(withdrawAmount)
+   const newWTotal = withdraConvetoNum + withdrayAmoutotNum;
+   withDrawBanalceText.innerText = newWTotal
+   
 
-}
+//    update balance 
 
+
+/* const balanceTotal =document.getElementById('balance-total')
+const balanceTotalText =balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(balanceTotalText)
+
+const newBalanceTotal = previousBalanceTotal + parseFloat(depositValue)
+
+balanceTotal.innerText =newBalanceTotal;
+depositInput.value = ''; */
+
+const balanceTotal =document.getElementById('balance-total')
+const balanceTotalText =balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(balanceTotalText)
+
+
+const newBalanceTotal = previousBalanceTotal - parseFloat(withdrawAmount)
+balanceTotal.innerText = newBalanceTotal;
+withdrawInput.value ='';
+
+})
 
